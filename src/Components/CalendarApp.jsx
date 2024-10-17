aa76ca7034d5e0d89bf497fdb1b09d643640a283
matthewgstillman <matthewgstillman@gmail.com>
2024-10-17 14:41:52 -0600

    additional calendar functionality

--- src/Components/CalendarApp.jsx
+++ src/Components/CalendarApp.jsx
@@ -11,18 +11,26 @@ const CalendarApp = () => {
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const firstDayOfMonth =  new Date(currentYear, currentMonth, 1).getDay();
 
-  console.log(daysInMonth, firstDayOfMonth);
+  const prevMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
+    setCurrentYear((prevYear) => (currentMonth === 0 ? prevYear - 1 : prevYear));
+  }
+
+  const nextMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1));
+    setCurrentYear((prevYear) => (currentMonth === 11 ? prevYear + 1 : prevYear));
+  }
 
   return (
     <div className="calendar-app">
       <div className="calendar">
         <h1 className="heading">Calendar</h1>
         <div className="navigate-date">
-          <h2 className="month">May,</h2>
-          <h2 className="year">2024</h2>
+          <h2 className="month">{monthsOfYear[currentMonth]},</h2>
+          <h2 className="year">{currentYear}</h2>
           <div className="buttons">
-            <i className="bx bx-chevron-left"></i>
-            <i className="bx bx-chevron-right"></i>
+            <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+            <i className="bx bx-chevron-right" onClick={nextMonth}></i>
           </div>
         </div>
         <div className="weekdays">
@@ -35,7 +43,8 @@ const CalendarApp = () => {
             <span key={`empty-${index}`}/>
           ))}
           {[...Array(daysInMonth).keys()].map((day) =>
-            <span key={day + 1}>{day + 1}</span>
+            <span key={day + 1} className={day + 1 === currentDate.getDate() && currentMonth === currentDate.getMonth() && 
+              currentYear == currentDate.getFullYear() ? 'current-day' : ''} >{day + 1}</span>
           )}
         </div>
       </div>
